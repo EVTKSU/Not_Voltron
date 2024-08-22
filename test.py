b20068f8ba92d0b05b1a18af2af3dd655ddcd821
buba <duxyn101@gmail.com>
2024-08-22 19:58:43 -0400

    just another tes

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("Helo World");
+#this is just a test
+class test:
+    def __init__(self, number):
+        self.number = number
+        
+    def write(self):
+        print(self.number)
+        
+
+n = test(5)
+n.write()
+print("Helo World")
